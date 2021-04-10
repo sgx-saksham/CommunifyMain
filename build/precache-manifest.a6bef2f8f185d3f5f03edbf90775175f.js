3be89e80f912ce8b6bcf7242a45b009e5662d6ae
Saksham Gupta <66645082+sgx-saksham@users.noreply.github.com>
2021-04-10 19:41:12 +0530

    Added Build folder

---
+++ build/precache-manifest.a6bef2f8f185d3f5f03edbf90775175f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be40a37146559d699d31d19d3a8c800",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1e12d731e871344bc90",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "84fbbac0a09e41c85e81",
+    "url": "/static/css/main.6995d314.chunk.css"
+  },
+  {
+    "revision": "d1e12d731e871344bc90",
+    "url": "/static/js/2.e0d7b1e6.chunk.js"
+  },
+  {
+    "revision": "e9b70ed94c92560626f6d789c6814c32",
+    "url": "/static/js/2.e0d7b1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fbbac0a09e41c85e81",
+    "url": "/static/js/main.846a6bf2.chunk.js"
+  },
+  {
+    "revision": "3f1ce5611be4b27d126b",
+    "url": "/static/js/runtime-main.4cc737fc.js"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  }
+]);
